32a999945865f28ea7c85ba5e96e8c00f42622e9
kasperbauer <chris@okeydoke.de>
2021-11-18 14:43:52 +0100

    format file

--- components/rich-text.vue
+++ components/rich-text.vue
@@ -1,48 +1,49 @@
 <template>
-  <div class="cms-content" :class="className" v-html="html" />
+    <div class="cms-content" :class="className" v-html="html" />
 </template>
 
 <script>
-  import PrismicDOM from 'prismic-dom'
-  import linkResolver from '~/util/linkResolver'
+import PrismicDOM from 'prismic-dom';
+import linkResolver from '~/util/linkResolver';
 
-  export default {
+export default {
     name: 'RichText',
     props: {
-      content: {
-        type: Array,
-        required: true,
-      },
-      className: {
-        type: String,
-        required: false
-      }
+        content: {
+            type: Array,
+            required: true
+        },
+        className: {
+            type: String,
+            required: false
+        }
     },
 
     computed: {
-      html() {
-        return PrismicDOM.RichText
-          .asHtml(this.content, linkResolver)
-          .replace(/<a([^>]*)>/g, (match, group) => {
-            return `<a${group} onclick="return goto(this)">`
-          })
-      },
+        html() {
+            return PrismicDOM.RichText.asHtml(
+                this.content,
+                linkResolver
+            ).replace(/<a([^>]*)>/g, (match, group) => {
+                return `<a${group} onclick="return goto(this)">`;
+            });
+        }
     },
 
-    mounted () {
-      if (window) {
-        window.goto = element => {
-          if (element.rel !== 'noopener') {
-            const href = element.getAttribute('href')
-            if (href.match(/^https?:\/\//)) {
-              return true
-            }
+    mounted() {
+        if (window) {
+            window.goto = element => {
+                if (element.rel !== 'noopener') {
+                    const href = element.getAttribute('href');
+                    if (href.match(/^https?:\/\//)) {
+                        return true;
+                    }
 
-            this.$router.push(href)
-            return false
-          }
+                    this.$router.push(href);
+                    return false;
+                }
+            };
         }
-      }
     }
-  }
+};
 </script>
